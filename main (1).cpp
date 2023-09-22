05b9bde31d6115dcb16617ae8f647adf5ae32981
Lovics <mihalyibalazs2002@gmail.com>
2023-09-22 18:47:01 +0200

    Kijavított kód

--- main (1).cpp
+++ main (1).cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];    // NELEMENTS rosszul van leírva
-    std::cout << '1-100 ertekek duplazasa' //Hiányzik egy pontosvessző
-    for (int i = 0;) //Hiányzik a ciklus feltételének a megadása
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //ciklus feltételének értéke mindig hamis, így a ciklus soha nem fut le.
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" //Hiányzik egy pontosvessző
-    }    
+        std::cout << "Ertek: " << b[i] << std::endl;
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; //A változót inicializálni kell
-    for (int i = 0; i < N_ELEMENTS, i++) //A vessző helyett pontosvesszőt kell használni
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    delete[] b; // Ne felejtsd el felszabadítani a dinamikusan foglalt memóriát
     return 0;
 }
